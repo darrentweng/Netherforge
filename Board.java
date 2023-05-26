96b70ac697cce0518d1900a039f70ea307e02adc
darrentweng <darren.t.weng@gmail.com>
2023-05-26 14:17:20 -0400

    making board work

--- Board.java
+++ Board.java
@@ -1,5 +1,5 @@
 public class Board {
-    private Block[][] visibleGrid;
+    private String[][] visibleGrid;
     private Block[][] mapGrid;
     private Item[][] itemGrid;
     private Mob[][] mobGrid;
@@ -12,7 +12,7 @@ public class Board {
         this.visibleWidth = visibleWidth;
         this.visibleHeight = visibleHeight;
         this.player = player;
-        visibleGrid = new Block[visibleHeight][visibleWidth];
+        visibleGrid = new String[visibleHeight][visibleWidth];
         mapGrid = new Block[mapHeight][mapWidth];
         itemGrid = new Item[mapHeight][mapWidth];
         mobGrid = new Mob[mapHeight][mapWidth];
@@ -46,36 +46,50 @@ public class Board {
         // Copy the relevant portion of the mapGrid to visibleGrid
         for (int row = 0; row < visibleHeight; row++) {
             for (int col = 0; col < visibleWidth; col++) {
-                visibleGrid[row][col] = mapGrid[startY + row][startX + col];
+                if (playerX == startX + col && playerY == startY + row) {
+                    visibleGrid[row][col] = "@";
+                }
+                else if (mobGrid[startY + row][startX + col] != null) {
+                    visibleGrid[row][col] = mobGrid[startY + row][startX + col].toString();
+                }
+                else if (itemGrid[startY + row][startX + col] != null) {
+                    visibleGrid[row][col] = itemGrid[startY + row][startX + col].toString();
+                }
+                else if (mapGrid[startY + row][startX + col] != null) {
+                    visibleGrid[row][col] = mapGrid[startY + row][startX + col].toString();
             }
+                else {
+                    visibleGrid[row][col] = ".";
+                }
         }
+    }
         
         // Code to draw the visible grid
         for (int row = 0; row < visibleGrid.length; row++) {
             for (int col = 0; col < visibleGrid[row].length; col++) {
-                if (row == player.getPlayerY() - startY && col == player.getPlayerX() - startX) {
+                /* if (row == player.getPlayerY() - startY && col == player.getPlayerX() - startX) {
                     System.out.print("@");
                 }
-                else if (mobGrid[row+startY][col+startX] != null) {
+                else if (mobGrid[row-startY][col-startX] != null) {
 
                     System.out.print("a");
                 }
-                else if (itemGrid[row+startY][col+startX] != null) {
-                    System.out.print(itemGrid[row+startY][col+startX]);
+                else if (itemGrid[row-startY][col-startX] != null) {
+                    System.out.print(itemGrid[row-startY][col-startX]);
                 }
-                else if (visibleGrid[row+startY][col+startX] == null) {
+                else if (visibleGrid[startY][startX] == null) {
                     System.out.print(".");
-                }
-                else {
+                }*/
+                
+                //else {
                     System.out.print(visibleGrid[row][col]);
-                }
+                //}
                 
                 
             }
             System.out.println();
         }
     }
-    
     
     public void setBlock(int x, int y, Block tile) {
         // Check if the given coordinates are within the bounds of the mapGrid
